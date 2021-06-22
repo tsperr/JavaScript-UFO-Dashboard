ca956db5f6d0add170d11ff07bc5d376ad52687a
Taylor Sperry <76749991+tsperr@users.noreply.github.com>
2021-06-22 16:51:41 -0400

    Update app.js

--- UFO-Level-1/app.js
+++ UFO-Level-1/app.js
@@ -2,3 +2,42 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var table = d3.select("table");
+var form = d3.select("#filters")
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+
+
+tableData.forEach(function(ufoData) {
+    console.log(ufoData);
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#filter-btn");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    console.log(tableData);
+  
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+  
+    console.log(filteredData)
+};
